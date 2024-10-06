4c5233cedd45f41c22379d3b3349998f0372795c
RobertFigenscher <robert.figenscher@upr.edu>
2024-10-06 13:51:34 +0000

    cambios al código

--- main.cpp
+++ main.cpp
@@ -2,10 +2,22 @@
 using namespace std;
 int main()
 {
-  for (int num1 = 10 num1 >= -10; num+=2)
-    cout << num1 << ", " ;
-  cout endl; 
+  for (int num1 = 10; num1 >= -10; num1 -= 2)
+     cout << num1 << ", " ;
+  cout << endl; 
   
+  for (int num2 = 1; num2 <= 22; num2 += 3) 
+   cout << num2 << ", ";
+  cout << endl; 
+
+  for (int num3 = 1; num3 <= 81; num3 *= 3)
+    cout << num3 << ", "; 
+  cout << endl; 
+
+  for (int num4 = 0; num4 <= 14; num4++)
+   if (num4 % 3 != 0 || num4 == 0) 
+    cout << num4 << ", ";
+  cout << endl;
   return 0;
 }
   
